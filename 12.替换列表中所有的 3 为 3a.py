7c35830535845cf977f1e4b417d1276ed0f91e0e
xixinsama <95458273+xixinsama@users.noreply.github.com>
2023-03-25 15:15:20 +0800

    Delete 12.替换列表中所有的 3 为 3a.py

--- 12.替换列表中所有的 3 为 3a.py
+++
@@ -1,7 +0,0 @@
-num = ["harden","lampard",3,34,45,56,76,87,78,45,3,3,3,87686,98,76]
-print(num.count(3))
-print(num.index(3))
-for i in range(num.count(3)): #获取 3 出现的次数
-    ele_index = num.index(3) #获取首次 3 出现的坐标
-    num[ele_index]="3a" #修改 3 为 3a
-print(num)
